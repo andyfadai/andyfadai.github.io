c5b1a44b7214fbd32b6b86c304933873d6f51cce
Yifei Wang <yifei.wang3@student.unimelb.edu.au>
2021-02-28 11:49:44 +1100

    Colour generater added

---
+++ CalcColor.js
@@ -0,0 +1,20 @@
+let btn = document.getElementById("change");
+let text = document.getElementById("colour");
+
+let generator = function() {
+      newColour = '#' +
+        (Math.random()*0xFFFFFF<<0).toString(16);
+        console.log(newColour.length);
+        if(newColour.length < 7) {
+            generator();
+        }
+}
+
+btn.addEventListener('click', function() {
+    generator();
+
+
+    document.body.style.background = newColour;
+    btn.style.color = newColour;
+    text.innerText = newColour;
+});
