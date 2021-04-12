91b80c84d9bfb81b1b083bdb45355db3ea5a4f53
Yifei Wang <yifei.wang3@student.unimelb.edu.au>
2021-04-12 10:04:37 +1000

    UI changed, version1.0

---
+++ jsFiles/CalcColor.js
@@ -0,0 +1,21 @@
+let btn = document.getElementById("change");
+let home = document.getElementById("home");
+let text = document.getElementById("colour");
+
+let newColor;
+let generator = function() {
+    newColor = '#' +
+        (Math.random()*0xFFFFFF<<0).toString(16);
+    console.log(newColor.length);
+    if(newColor.length < 7) {
+        generator();
+    }
+}
+
+btn.addEventListener('click', function() {
+    generator();
+    document.body.style.background = newColor;
+    btn.style.color = newColor;
+    home.style.color = newColor;
+    text.innerText = newColor;
+});
